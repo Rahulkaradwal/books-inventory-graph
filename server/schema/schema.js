5da56566cdbee6a89044897304314936503b7158
rahul karadwal <rahulkaradwal2000@gmail.com>
2024-12-18 19:26:19 -0500

    feat: author type and query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,12 @@
 // const { query } = require("express");
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 const _ = require("lodash");
 
 // dummy data
@@ -18,12 +24,34 @@ const books = [
   { id: "10", name: "Brave New World", genre: "Science Fiction" },
 ];
 
+const authors = [
+  { id: "1", name: "Harper Lee", age: 89 },
+  { id: "2", name: "George Orwell", age: 46 },
+  { id: "3", name: "F. Scott Fitzgerald", age: 44 },
+  { id: "4", name: "J.D. Salinger", age: 91 },
+  { id: "5", name: "Herman Melville", age: 72 },
+  { id: "6", name: "Jane Austen", age: 41 },
+  { id: "7", name: "J.R.R. Tolkien", age: 81 },
+  { id: "8", name: "J.K. Rowling", age: 58 },
+  { id: "9", name: "Paulo Coelho", age: 76 },
+  { id: "10", name: "Aldous Huxley", age: 69 },
+];
+
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -39,6 +67,13 @@ const RootQuery = new GraphQLObjectType({
         return _.find(books, { id: args.id });
         // vanilla js approach
         // return books.find((book) => book.id === args.id);
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
       },
     },
   },
